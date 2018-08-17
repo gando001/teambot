dff6b24b192c400503526d7d41b074452a851187
Kunal Madhav <kunal.madhav@xero.com>
2018-08-17 14:27:31 +1200

    Fix broken stand up command help

--- commands/stand_up.rb
+++ commands/stand_up.rb
@@ -1,21 +1,18 @@
 require_relative '../services/stand_up'
 
-module SlackRubyBot
-  module Commands
-    class Standup < Base
-      command 'stand up'
-      command 'standup'
+class Standup < SlackRubyBot::Commands::Base
+  command 'stand up'
+  command 'standup'
 
-      help do
-        title 'stand up or standup'
-        desc 'Let\'s you know when the next stand up is and who will be leading it'
-      end
+  help do
+    title 'stand up'
+    desc 'Let\'s you know when the next stand up is and who will be leading it'
+    long_desc 'Randomly assigns a team member to lead the next stand up.'
+  end
 
-      def self.call(client, data, match)
-        message = StandUp.new.call
+  def self.call(client, data, match)
+    message = StandUp.new.call
 
-        client.say(text: message, channel: data.channel)
-      end
-    end
+    client.say(text: message, channel: data.channel)
   end
 end
