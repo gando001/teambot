c17906a82482419654dd298613db4a3e52a1fcf1
Kunal Madhav <kunal.madhav@xero.com>
2018-08-20 09:26:33 +1200

    Add help to where is command

--- commands/google.rb
+++ commands/google.rb
@@ -3,6 +3,12 @@ require_relative '../services/google'
 class GoogleCalendar < SlackRubyBot::Commands::Base
   command 'where is'
 
+  help do
+    title 'where is'
+    desc 'Displays events from the given persons calendar'
+    long_desc 'Looking for someone in your team? Use this command to find where their are now or what events they have today'
+  end
+
   def self.call(client, data, match)
     message = Calendar.new(match[:expression]).call
 
